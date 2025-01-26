c7658264c3205c848974a3a91ae417a86f5c17a9
yuuta-git12 <tayu2201@icloud.com>
2025-01-26 12:13:46 +0900

    [add]axiosを使ってjsonplaceholderから仮データを取得する機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const onClickFetchData = () => {
+    // acxiosを使ってjsonplaceholderから仮のデータを取得
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res)=>{
+      console.log(res);
+    });
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+    </div>
+  );
+    
 }
-
-export default App
