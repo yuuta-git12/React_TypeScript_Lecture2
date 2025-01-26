dc81bdd56e94ed5d03d27bf0d1b8e0d2ea208238
yuuta-git12 <tayu2201@icloud.com>
2025-01-26 12:35:23 +0900

    [update]Stateとaxiosに型定義を実装してデータを取得するように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
+import { Todo } from './Todo';
+
+// jsonplaceholderから取ってくるデータの型定義
+type TodoType = {
+    "userId": number;
+    "id": number;
+    "title": string;
+    "completed": boolean;
+};
 
 export default function App() {
+  // 画面にデータを表示するためのStateの宣言(型定義はTodoTypeを定義)
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    // acxiosを使ってjsonplaceholderから仮のデータを取得
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res)=>{
-      console.log(res);
+    // axiosを使ってjsonplaceholderから仮のデータを取得(型定義はTodoTypeを定義)
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+    .then((res)=>{
+      setTodos(res.data);
     });
   };
   
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId}/>
+      ))}
     </div>
   );
     
